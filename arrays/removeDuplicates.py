f1921a6d44a5abb96442cecdb1eef4447164cfd8
quantum-sage <65615807+quantum-sage@users.noreply.github.com>
2024-11-01 18:18:24 +0530

    removeDuplicates.py

--- arrays/removeDuplicates.py
+++ arrays/removeDuplicates.py
@@ -1,2 +1,18 @@
-def removeDuplicates(self, nums):
+class Solution:
+    def removeDuplicates(self, nums):
+        unique_values = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1]:
+                nums[unique_values]= nums[i]
+                unique_values+=1
+        return unique_values
+
+def main():
+    nums = [1, 1, 2, 2, 3, 4, 4]
+    solution = Solution()
+    unique_count = solution.removeDuplicates(nums)
+    print(f"Number of unique elements: {unique_count}")
+    print(f"Array after removing duplicates: {nums[:unique_count]}")
+main()
     
+
